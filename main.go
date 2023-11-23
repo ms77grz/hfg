6d12bd48d1a3632d1d5ccd9d8f264278d23589fa
Magomed Shamaev <ms77grz@gmail.com>
2023-11-24 00:45:33 +0300

    HFG: Maps

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
-import "fmt"
-
-func inRange(min float64, max float64, numbers ...float64) []float64 {
-	var result []float64
-	for _, number := range numbers {
-		if number >= min && number <= max {
-			result = append(result, number)
-		}
-	}
-	return result
-}
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(inRange(1, 100, -12.5, 3.2, 0, 50, 103.5))
-	fmt.Println(inRange(-10, 10, 4.1, 12, -12, -5.2))
+	// var ranks map[string]int
+	// ranks = make(map[string]int)
+	ranks := make(map[string]int)
+	ranks["gold"] = 1
+	ranks["silver"] = 2
+	ranks["bronze"] = 3
+	fmt.Println(ranks["bronze"])
+	fmt.Println(ranks["gold"])
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["Li"] = "Lithium"
+	fmt.Println(elements["Li"])
+	fmt.Println(elements["H"])
+
+	isPrime := make(map[int]bool)
+	isPrime[4] = false
+	isPrime[7] = true
+	fmt.Println(isPrime[4])
+	fmt.Println(isPrime[7])
 }
