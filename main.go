0210049c6967147b6273c25b2925622f1155a0ad
ms77grz <ms77grz@gmail.com>
2023-11-28 18:55:12 +0300

    HFG: Error handling in a recursive function+

--- main.go
+++ main.go
@@ -2,50 +2,14 @@ package main
 
 import "fmt"
 
-type Car string
-
-func (c Car) Accelerate() {
-	fmt.Println("Speeding up")
-}
-
-func (c Car) Brake() {
-	fmt.Println("Stopping")
-}
-
-func (c Car) Steer(direction string) {
-	fmt.Println("Turning", direction)
-}
-
-type Truck string
-
-func (t Truck) Accelerate() {
-	fmt.Println("Speeding up")
-}
-
-func (t Truck) Brake() {
-	fmt.Println("Stopping")
-}
-
-func (t Truck) Steer(direction string) {
-	fmt.Println("Turning", direction)
-}
-
-func (t Truck) LoadCargo(cargo string) {
-	fmt.Println("Loading", cargo)
-}
-
-type Vehicle interface {
-	Accelerate()
-	Brake()
-	Steer(string)
+func count(start int, end int) {
+	fmt.Println(start)
+	if start < end {
+		count(start+1, end)
+	}
+	fmt.Printf("Returning from count(%d, %d) call\n", start,)
 }
 
 func main() {
-	var vehicle Vehicle = Car("Toyota Yaris")
-	vehicle.Accelerate()
-	vehicle.Steer("left")
-
-	vehicle = Truck("Ford F180")
-	vehicle.Brake()
-	vehicle.Steer("right")
+	count(1, 3)
 }
